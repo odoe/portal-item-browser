1d6196eef7eb1db2fab019f9976d81b105b4520a
rene rubalcava <rrubalcava@esri.com>
2015-12-07 07:55:36 -0800

    some cleanup

--- src/app/main.ts
+++ src/app/main.ts
@@ -16,24 +16,41 @@ declare var Portal : any;
 declare var PortalQueryParams : any;
 declare var esriRequest : any;
 
+const $ = document.querySelector.bind(document);
+
+interface PortalItem {
+  id : string,
+  title : string,
+  thumbnailUrl : string,
+  description : string,
+  accessInformation : string,
+  itemUrl : string,
+  owner : string,
+  snippet : string,
+  avgRating : number,
+  selected : boolean,
+  showmap : boolean
+}
+
 function container(): HTMLElement {
   let node = document.createElement("div");
   document.body.appendChild(node);
   return node;
 }
 
-function asJSON(item) {
-  return { id : item.id || ""
-  , title : item.title || ""
-  , thumbnailUrl : item.itemUrl + "/info/" + item.thumbnail
-  , description : item.description || ""
-  , accessInformation : item.accessInformation || "Not available"
-  , itemUrl : item.itemUrl || ""
-  , owner: item.owner || ""
-  , snippet : item.snippet || ""
-  , avgRating : item.avgRating || 0.0
-  , selected : false
-  , showmap : false
+function asJSON(item) : PortalItem {
+  return {
+    id : item.id || "",
+    title : item.title || "",
+    thumbnailUrl : `${item.itemUrl}/info/${item.thumbnail}`,
+    description : item.description || "",
+    accessInformation : item.accessInformation || "Not available",
+    itemUrl : item.itemUrl || "",
+    owner: item.owner || "",
+    snippet : item.snippet || "",
+    avgRating : item.avgRating || 0.0,
+    selected : false,
+    showmap : false,
   };
 }
 
@@ -47,11 +64,24 @@ const query = new PortalQueryParams({ query: 'type:"Vector Tile Service"' });
 
 const mainNode: HTMLElement = container();
 
-const elmApp: any = Elm.embed(Elm.PortalItems, mainNode, { portalItems: { items: [] }});
+const elmApp = Elm.embed(Elm.PortalItems, mainNode, { portalItems: { items: [] }});
 
 const updateItems = (items) => elmApp.ports.portalItems.send({ items });
 
 const dealwithit = ({ results }) => updateItems(results.map(asJSON));
+
+const createMap = (model) => () => {
+  map = new Map();
+  view = new MapView({
+    container: $(".view-div"),
+    map: map,
+    center: [-100.33, 25.69],
+    zoom: 10
+  });
+  map.add(new VectorTileLayer({
+    url: `${model.itemUrl}/resources/styles/root.json`
+  }));
+};
 
 // subscribe to Elm app model updates
 elmApp.ports.modelChanges.subscribe(model => {
@@ -66,22 +96,8 @@ elmApp.ports.modelChanges.subscribe(model => {
   let forShow = model.items.filter(x => x.showmap);
   if (forShow.length) {
     let model = forShow.shift();
-    // DOM not complete when this signal
-    // fires. Need a slight delay
-    setTimeout(() => {
-      let node = document.querySelector(".view-div");
-      map = new Map();
-      view = new MapView({
-        container: node,
-        map: map,
-        center: [-100.33, 25.69],
-        zoom: 10
-      });
-      let tileLyr = new VectorTileLayer({
-        url: model.itemUrl + "/resources/styles/root.json"
-      });
-      map.add(tileLyr);
-    }, 500);
+    // DOM not complete when this signal fires
+    window.requestAnimationFrame(createMap(model));
   }
 });
 
